c68a93dabfb0abab1321881667a35fd3274837ef
Mognom <alex.mognom@gmail.com>
2015-07-20 18:26:46 +0200

    Implemented the block types and fixed its angle on placement

--- Assets/Mapa/LevelGen/mapGen.cs
+++ Assets/Mapa/LevelGen/mapGen.cs
@@ -11,20 +11,27 @@ using System.Collections;
  * 0 = BLOQUE BASE
  * 1 = PATH
  * 2 = BLOQUE BLOQUEADO
- * 3 = BORDE VERTICAL
- * 4 = BORDE HORIZONTAL
- * 5 = BORDE EXTERIOR
- * 6 = BORDE INTERIOR
+ * 3 = BORDE VERTICAL *2
+ * 4 = BORDE HORIZONTAL *2
+ * 5 = BORDE EXTERIOR *4
+ * 6 = BORDE INTERIOR *4
  * 
  */
 public class mapGen : MonoBehaviour {
 
 	//Definicion del mapa
-	private int[,] map = {{0,5,4,5},
+	private int[,] map = {{5,3,3,3,3,5},
+						  {3,1,1,1,1,3},
+						  {3,1,6,6,1,0},
+						  {3,1,6,6,1,0},
+						  {3,1,1,1,1,3},
+						  {5,3,3,3,3,5}}; 
+
+						/*{{0,5,4,5},
 						  {0,3,1,3},
 						  {5,6,1,3},
 						  {3,1,1,3},
-						  {5,4,4,5}};
+						  {5,4,4,5}};*/
 
 	//Bloques del mapa
 	public GameObject block, bloquedBlock;
@@ -44,45 +51,85 @@ public class mapGen : MonoBehaviour {
 				switch(map[z,x]){
 					//Bloque base
 					case 0:
-						actual = (GameObject) Instantiate (block,new Vector3(x,0,map.GetLength(0)-z -1),block.transform.rotation);
+						actual = (GameObject) Instantiate (block,new Vector3(x,0,map.GetLength(0)-z -1)*2,block.transform.rotation);
 						actual.name = block.name; //Odio el (clone) =3
 						break;
 
 					//Camino enemigos
 					case 1:
-						actual = (GameObject) Instantiate (path,new Vector3(x,0,map.GetLength(0)-z -1),path.transform.rotation);
+						actual = (GameObject) Instantiate (path,new Vector3(x,0,map.GetLength(0)-z -1)*2,path.transform.rotation);
 						actual.name = path.name; //Odio el (clone) =3
 						break;
 						
 					//Bloque bloqueado
 					case 2:
-						actual = (GameObject) Instantiate (bloquedBlock, new Vector3(x,0,map.GetLength(0)-z -1), bloquedBlock.transform.rotation);
+						actual = (GameObject) Instantiate (bloquedBlock, new Vector3(x,0,map.GetLength(0)-z -1)*2 , bloquedBlock.transform.rotation);
 						actual.name = bloquedBlock.name; //Odio el (clone) =3
 						break;
-	
-					//Borde vertical
+
+					//Borde
 					case 3:
-						actual = (GameObject) Instantiate (border,new Vector3(x,0,map.GetLength(0)-z -1),border.transform.rotation);
-						actual.name = border.name; //Odio el (clone) =3
-						break;
-					
-					//Borde horizontal
-					case 4:
 						rotation = border.transform.rotation;
-						rotation.eulerAngles.Set(0,90,0);
-						actual = (GameObject) Instantiate (border,new Vector3(x,0,map.GetLength(0)-z -1), rotation);
+						if(x+1 < map.GetLength(1) && map[z,x+1] == 1)
+							rotation = Quaternion.Euler(-90,-90,0);
+						else
+							if(x-1 > 0 && map[z,x-1] == 1)
+								rotation = Quaternion.Euler(-90,90,0);
+							else
+								if(z+1 < map.GetLength(0) && map[z+1,x] == 1)
+									rotation = Quaternion.Euler(-90,0,0);
+								else
+									rotation = Quaternion.Euler(-90,180,0);
+						
+						actual = (GameObject) Instantiate (border,new Vector3(x,0,map.GetLength(0)-z -1) *2 ,rotation);
 						actual.name = border.name; //Odio el (clone) =3
 						break;
 				
 					//Borde exterior
 					case 5:
-						actual = (GameObject) Instantiate (exteriorBorder,new Vector3(x,0,map.GetLength(0)-z -1),exteriorBorder.transform.rotation);
+						rotation = border.transform.rotation;
+						//Esquina derecha arriba
+						if(x+1 < map.GetLength(1) && z+1 < map.GetLength(0) && map[z+1,x+1] == 1)
+							rotation = Quaternion.Euler(-90,270,0);
+						
+						//Esquina derecha abajo
+						else if(x+1 < map.GetLength(1) && z-1 > 0 && map[z-1,x+1] == 1)
+							rotation = Quaternion.Euler(-90,180,0);
+						
+						//Esquina izquierda arriba
+						else if(x-1 > 0 && z+1 < map.GetLength(0) && map[z+1,x-1] == 1)
+							rotation = Quaternion.Euler(-90,0,0);
+						
+						//Esquina izquierda abajo
+						else if(x-1 > 0 && z-1 > 0 && map[z-1,x-1] == 1)
+							rotation = Quaternion.Euler(-90,90,0);
+						
+						actual = (GameObject) Instantiate (exteriorBorder,new Vector3(x,0,map.GetLength(0)-z -1)*2 ,rotation);
 						actual.name = exteriorBorder.name; //Odio el (clone) =3
 						break;
 					
-					//Bordo interior
+					
+					//Borde interior
 					case 6:
-						actual = (GameObject) Instantiate (interiorBorder,new Vector3(x,0,map.GetLength(0)-z -1),interiorBorder.transform.rotation);
+						rotation = border.transform.rotation;
+						//Esquina derecha arriba
+						if(x+1 < map.GetLength(1) && z+1 < map.GetLength(0) && map[z+1,x] == 1 && map[z,x+1] == 1)
+							rotation = Quaternion.Euler(-90,0,0);
+						
+						//Esquina derecha abajo
+						else if(x+1 < map.GetLength(1) && z-1 > 0 && map[z-1,x] == 1 && map[z,x+1] == 1)
+							rotation = Quaternion.Euler(-90,270,0);
+						
+						//Esquina izquierda arriba
+						else if(x-1 > 0 && z+1 < map.GetLength(0) && map[z+1,x] == 1 && map[z,x-1] == 1)
+							rotation = Quaternion.Euler(-90,90,0);
+						
+						//Esquina izquierda abajo
+						else if(x-1 > 0 && z-1 > 0 && map[z-1,x] == 1 && map[z,x-1] == 1)
+							rotation = Quaternion.Euler(-90,180,0);
+						
+						
+						actual = (GameObject) Instantiate (interiorBorder,new Vector3(x,0,map.GetLength(0)-z -1)*2, rotation);
 						actual.name = interiorBorder.name; //Odio el (clone) =3
 						break;
 
