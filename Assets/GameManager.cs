7d2719ffd95ac89f1ef2903ec35aacd919c29b15
Mognom <alex.mognom@gmail.com>
2015-07-21 17:29:19 +0200

    Basic Construction
    basics of construction.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	/*
+	 * 0 -> nada 
+	 * 1 -> arquero
+	 * ...
+	 * 
+	 */
+	private int selected;
+	public GameObject torre;
+	
+	// Use this for initialization
+	void Start () {
+		selected = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public bool interaccionBloque(Vector3 pos){
+		
+		print ("se ha hecho click en un bloque");
+		
+		
+		//DETECTAR SI CONSTRUIR O NO
+		switch (selected) {
+			//Construir torre de arqueros
+		case 1:
+			claridad(pos);
+			break;
+			
+			
+		}
+		
+		return false;
+		
+		
+	}
+	
+	private void claridad(Vector3 pos){
+		GameObject actual;
+		actual = (GameObject) Instantiate (torre, pos + new Vector3(0f,0.3f,0f) , torre.transform.rotation);
+		
+		
+		
+	}
+	public void setSelected(int id){
+		selected = id;
+	}
+}
---
+++ Assets/Mapa/Block/blockClickDetection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockClickDetection : MonoBehaviour {
+	
+	private GameManager gm;
+	private bool used;
+
+	void Start () {
+		gm = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+
+		used = false;
+	}
+
+
+	void OnMouseOver(){
+		if (!used && Input.GetMouseButtonDown (0)) {
+			used = !gm.interaccionBloque(transform.position);
+
+		}
+	}
+
+}
