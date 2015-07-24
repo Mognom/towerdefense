74fdd5f06b02032f7efdd82b01108768bb768b25
Mognom <alex.mognom@gmail.com>
2015-07-24 23:10:53 +0200

    Beta- not working yet

---
+++ Assets/Mapa/CornerTurn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornerTurn : MonoBehaviour {
+
+	// Use this for initialization
+
+	private int direction = 1; // 1 = 90; -1 = -90
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		//Destroy (other.gameObject);
+		Vector3 actual = other.attachedRigidbody.velocity;
+		print (actual.ToString());
+		other.attachedRigidbody.velocity = Vector3.zero;
+		//other.attachedRigidbody.AddForce(new Vector3 (0, 0, 1));
+
+		
+	}
+
+	public void setDirection (int dir){
+		direction = dir;
+
+
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3 (0, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
