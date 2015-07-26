cd725590da14e3ab64c7468b1c0ca9a5a97713e4
Mognom <alex.mognom@gmail.com>
2015-07-26 13:12:42 +0200

    Basic Targeting
    Closest target until leaves range

---
+++ Assets/Test/Muevete.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Muevete : MonoBehaviour {
+
+	// Use this for initializationç
+	public int speed;
+	void Start () {
+		this.GetComponent<Rigidbody> ().velocity = new Vector3 (speed, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Towers/Target.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	public string enemiesTag;
+	public double range;
+
+	private GameObject target;
+	private Vector2 pos;
+	void Start () {
+		pos = new Vector2(transform.position.x,transform.position.z);
+		findClosest ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null) 
+			target = findClosest ();
+		else if (Vector2.Distance (new Vector2 (target.transform.position.x, target.transform.position.z), pos) > range) {
+			target.GetComponent<MeshRenderer> ().material.color = new Color (1, 1, 1, 1);
+			target = null;
+		}
+		else {
+				print ("tiene objetivo");
+				target.GetComponent<MeshRenderer> ().material.color = new Color (1, 0, 1, 1);
+
+		}
+	}
+
+
+
+
+	private GameObject findClosest(){
+		GameObject result = null;
+		GameObject[] list = GameObject.FindGameObjectsWithTag (enemiesTag);
+
+		double distancia;
+		double menorActual = range+1;
+
+
+		foreach (GameObject enemy in list) {
+			distancia = Vector2.Distance(new Vector2(enemy.transform.position.x,enemy.transform.position.z), pos);
+			if( distancia <= range && distancia < menorActual){
+				result = enemy;
+				menorActual = distancia;
+			}
+		}
+		return result;
+	}
+}
