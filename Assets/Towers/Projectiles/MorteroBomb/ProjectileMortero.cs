e84680e6c27c1c91ae9b5ad5e760e49722a53a3b
Mognom <alex.mognom@gmail.com>
2015-08-04 21:17:39 +0200

    First attemp at parabolic throw
    Failed, needs speeds adjustment for it to not look weird

---
+++ Assets/Towers/Projectiles/MorteroBomb/ProjectileMortero.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMortero : Projectile {
+
+	// Use this for initialization
+	private Vector2 destino;
+	private Vector3 subida;
+
+	//Malditos tiros parabolicos
+	private float time;
+	private float y0;
+
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 posXZ;
+		float posY;
+		float step = speed * Time.deltaTime;
+		time += Time.deltaTime;
+		//x = x0 + v0*t + 1/2 * g *t --> posicion lanzamiento vertical
+		posY = y0 + speed * time - 0.5f * Physics.gravity.magnitude * time * time;
+
+		posXZ = Vector2.MoveTowards (new Vector2(transform.position.x, transform.position.z), new Vector2(objective.transform.position.x, objective.transform.position.z), step);
+
+		//Avanza siguiendo la parabola planteada
+		transform.position = new Vector3 (posXZ.x, posY, posXZ.y);
+		//transform.position = Vector3.MoveTowards (transform.position, destino, step);
+		
+	}
+	
+	public override void setObjective(GameObject o){
+		objective = o;
+		time = 0;
+		y0 = transform.position.y;
+		destino = new Vector2 (o.transform.position.x, o.transform.position.z);
+
+	}
+
+	//KABOOOOOM
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Enemy") {
+			other.gameObject.GetComponent<EnemyAI>().damaged(damage);
+			//this.gameObject.recycle();
+		}
+		
+	}
+}
