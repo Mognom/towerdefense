cf5211b9c0e72d06e9de4790fafd5f14f245680d
Mognom <alex.mognom@gmail.com>
2015-08-09 13:21:16 +0200

    Main Menu workingeru
    Exit only works on build tho

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	//Singleton powa
+	private static GameManager instance;
+
+	private int currentScene;
+
+	void Start () {
+		//SOLO PUEDE QUEDAR UNO
+		if (instance == null) {
+			instance = this;
+			//No se destruye jamas de los jamases
+			DontDestroyOnLoad (this.gameObject);
+
+		}
+		else
+			Destroy (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void play(){
+		print ("hola");
+		currentScene = 1;
+		Application.LoadLevel (1);
+	}
+
+	//Cierra el juego ?
+	public void exit(){
+		print ("Hola mundo");
+		Application.Quit ();
+
+
+	}
+
+}
